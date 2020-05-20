40c51f00eed70e6b3750714fd0f2a90738c581e8
Olya Savliuk <olya.dytkevuch@gmail.com>
2020-05-20 15:23:06 +0300

    Questionnaire program: the first correction

--- js/olya.js
+++ js/olya.js
@@ -4,14 +4,11 @@ var job = prompt('What is your job?');
 var favoriteAuthor = prompt('Who is your favorite author?');
 var favoriteColor = prompt('What\'s your favorite color?');
 var message = 'Hello '+ firstName + ' ! I see you ' + age + ' years old,';
-var canVote;
 
 if (age >= 18) {
-  canVote = true;
   message += ' as I can see, you could vote already.';
 }
 else {
-  canVote = false;
   message += ' as I can see, you couldn\' vote yet.';
 }
 
